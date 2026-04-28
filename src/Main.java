77748a857206288adf33966a3c8fa68ce78a0f9f
llNestyll <llnestyll@ro.ru>
2026-04-28 21:26:51 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+        int age = 17;
+        if (age >= 18) {
+        System.out.println("ты совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("ты еще не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        int ageq = 19;
+        if (ageq >= 18){
+            System.out.println("ты совершеннолетний");
+        }
+        if (ageq < 18){
+            System.out.println("ты еще не достиг совершеннолетия, нужно немного подождать");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
